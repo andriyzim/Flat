0b7d640f1a164a2fda9ddad1f047b64b8a47e88a
andriyzim <andriy_zimbitski@ukr.net>
2023-11-24 17:46:46 +0200

    initial

---
+++ Assets/VrHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VrHelper : MonoBehaviour
+{
+    LaserPointer lp;
+    LineRenderer lr;
+
+    public LaserPointer.LaserBeamBehavior laserBeamBehavior;
+
+    void Awake()
+    {
+        lp = FindObjectOfType<LaserPointer>();
+        GetComponent<OVRRaycaster>().pointer = lp.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
